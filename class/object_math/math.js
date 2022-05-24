aa94737e34840e40ba07ecec5ba954dfec02a7b7
Vittorio Adesso <adessovittorio8@gmail.com>
2022-05-24 09:28:53 +0200

    Math

--- class/object_math/math.js
+++ class/object_math/math.js
@@ -4,3 +4,19 @@ console.log(pi);
 // min & max
 console.log(Math.min(1,5,8,9,2,-2,-1,10)); 
 console.log(Math.max(1,5,8,9,2,-2,-1,10)); 
+
+// redondear
+
+console.log(Math.round(46.5)); 
+console.log(Math.round(46.4)); 
+// redondea hacia abajo
+console.log(Math.floor(46.8)); 
+// redondeo hacia arriba
+console.log(Math.ceil(46.8)); 
+
+
+// generate alleatories numbers ( between 0 & 1 )
+let aleatories = Math.ramdon();
+console.log(aleatories); 
+
+
